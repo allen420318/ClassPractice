1d906339dd5879dab5bbf923bb9023c76b143176
user <allen420318@gmail.com>
2018-06-30 15:39:00 +0800

    2018063001

--- WGPD03_02/src/app02.js
+++ WGPD03_02/src/app02.js
@@ -2,7 +2,7 @@ var Main02Layer = cc.Layer.extend({
     sprite:null,
     sx:1,
     sy:1,
-    player1: new Array(13),
+    players: new Array(new Array(13),new Array(13),new Array(13),new Array(13)),
     ctor:function () {
         this._super();
 
@@ -10,14 +10,18 @@ var Main02Layer = cc.Layer.extend({
         this.sprite = new cc.Sprite("#pokers_back.png");
         console.log(this.sprite.width + ":" + this.sprite.height);
         this.sx = (cc.winSize.width / 13) / (this.sprite.width+24);
-        this.sy = (cc.winSize.height / 4) / (this.sprite.height+48);
+        this.sy = (cc.winSize.height / 4) / (this.sprite.height+60);
 
-        for (var i = 0; i<this.player1.length; i++){
-            this.player1[i] = new cc.Sprite("#pokers_back.png");
-            this.player1[i].x = cc.winSize.width *(i+1) / 14;
-            this.player1[i].y = cc.winSize.height * 4/5;
-            this.player1[i].setScale(this.sx,this.sy);
-            this.addChild(this.player1[i]);
+
+        for (var j = 0; j<this.players.length; j++){
+
+            for (var i = 0; i<this.players[j].length; i++){
+                this.players[j][i] = new cc.Sprite("#pokers_back.png");
+                this.players[j][i].x = cc.winSize.width *(i+1) / 14;
+                this.players[j][i].y = cc.winSize.height * (j+1) /5;
+                this.players[j][i].setScale(this.sx,this.sy);
+                this.addChild(this.players[j][i]);
+            }
         }
 
         this.sprite.x = cc.winSize.width *1 /14;

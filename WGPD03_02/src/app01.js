6823a5613a06f86a398a0753163bd0cad6f5bbef
user <allen420318@gmail.com>
2018-06-30 14:51:19 +0800

    2018063001

--- WGPD03_02/src/app01.js
+++ WGPD03_02/src/app01.js
@@ -2,6 +2,8 @@ var MainLayer = cc.Layer.extend({
     p2:null,
     status:null,
     isOn: false,
+    poker7:null,
+    poker4:null,
     ctor:function () {
 
         this._super();
@@ -19,7 +21,25 @@ var MainLayer = cc.Layer.extend({
             y: cc.winSize.height *7 /8
         });
         this.addChild(this.status);
-        
+
+        this.poker7 = new cc.Sprite(res.poker_png,cc.rect(221,711,220,218));
+        this.poker7.attr({
+            x: cc.winSize.width /4,
+            y: cc.winSize.height /2,
+            rotation: -90,
+        });
+        this.addChild(this.poker7);
+
+        cc.spriteFrameCache.addSpriteFrames(res.poker_plist,res.poker_png);
+
+        this.poker4 = new cc.Sprite("#pokers_s04.png");
+        this.poker4.attr({
+            x: cc.winSize.width *3 /4,
+            y: cc.winSize.height /2,
+        });
+        this.poker4.setScale(0.5,0.5);
+        this.addChild(this.poker4);
+
         this.myMouseListener(this);
         
         return true;
@@ -29,7 +49,8 @@ var MainLayer = cc.Layer.extend({
         cc.eventManager.addListener({
             event: cc.EventListener.MOUSE,
             onMouseDown:function (event) {
-                
+                layer.isOn = !layer.isOn;
+                layer.status.setTexture(layer.isOn?res.status_on:res.status_off);
             }
             
         },layer);
